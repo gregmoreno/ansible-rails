274ddb5dfdf447dda53f705d20bb57758f3069d6
Greg Moreno <greg.moreno@gmail.com>
2014-08-05 08:52:22 -0700

    User ruby-install + chruby

--- roles/chruby/templates/chruby.sh
+++ roles/chruby/templates/chruby.sh
@@ -1,6 +1,6 @@
 source /usr/local/share/chruby/chruby.sh
 source /usr/local/share/chruby/auto.sh
-chruby "{{ ruby_version }}"
+chruby ruby-{{ ruby_version }}
 
 if [ -n "$BASH_VERSION" ] || [ -n "$ZSH_VERSION" ]; then
   source /usr/local/share/chruby/chruby.sh
